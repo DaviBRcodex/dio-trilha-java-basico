447fd6998c4421bf24dec23a2c129da6f7cee463
DaviBRcodex <db2017335@gmail.com>
2024-04-27 17:25:34 -0300

    Neste exemplo fui atrás de documentar o java através do JAVADOC da IDE intellij

---
+++ Javinha/src/Calculadora.java
@@ -0,0 +1,24 @@
+/**
+ * <h1>Calculadora</h1>
+ *  A calculadora  realiza operações matemáticas entre numeros inteiros
+ *  <p>
+ *  <b>Note:</b> Leia atentamente a documentação desta classe
+ *  para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @autor  Davi Barbosa
+ * @version 1.0
+ * @since 27/04/2024
+ */
+
+ public class Calculadora{
+     /**
+      * Este método é utilizado para somar dois números inteiros
+      * @param numeroUm este é o primeiro parâmetro do método
+      * @param  numeroDois este é o segundo parãmetro do método
+      * @return int o resultado deste método é a soma dos dois números.
+      */
+      public int somar(int numeroUm, int numeroDois){
+          return numeroUm + numeroDois;
+      }
+
+}
